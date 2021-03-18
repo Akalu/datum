b6acddb61b776e4756cf30b352df5944e3d5cf1d
Aliaksei Kaliutau <aliaksei.kaliutau@gmail.com>
2021-03-18 17:03:44 +0300

    cleaned up

--- src/main/java/org/datum/generator/Generator.java
+++ src/main/java/org/datum/generator/Generator.java
@@ -7,11 +7,11 @@ import java.util.function.Supplier;
 public abstract class Generator<T> {
 
 	@SuppressWarnings("unchecked")
-	protected void set(Consumer<T> setter, String key, Map<String, Object> data) {
+	protected <T> void set(Consumer<T> setter, String key, Map<String, Object> data) {
 		setter.accept((T) data.get(key));
 	}
 	
-	protected void set(Consumer<T> setter, Supplier<T> dataSource) {
+	protected <T> void set(Consumer<T> setter, Supplier<T> dataSource) {
 		setter.accept(dataSource.get());
 	}
 
